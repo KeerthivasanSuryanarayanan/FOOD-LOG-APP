73af0ee66ecd9a5907b481fe5ce6b2b1aac8b1e7
Keerthivasan Suryanarayanan <104590925+KeerthivasanSuryanarayanan@users.noreply.github.com>
2022-11-08 00:43:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, UploadFile, File
+import json
+import pandas as pd
+import requests
+from PIL import Image, ImageOps
+import numpy as np
+import random
+from keras.models import load_model
+
+
+
+app = FastAPI()
+
+
+@app.post("/upload_image")
+async def create_file(file: UploadFile=File(...)):
+    file2store = await file.read()
+    f = open("input.png", "wb")
+    f.write(file2store)    
+    image_path = "input.png"
+    model = load_model('keras_model.h5')
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = Image.open(image_path).convert('RGB')
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+    class_names = ['French Fries', 'Ice Cream', 'Pizza', 'Fried Rice', 'Sushi', 'Curry']
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+    return {'class':class_name, 'confidence': str(confidence_score)}
