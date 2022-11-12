1053800ee8f02011b54e9fb6a9a885e9e2567533
Keerthivasan Suryanarayanan <104590925+KeerthivasanSuryanarayanan@users.noreply.github.com>
2022-11-12 08:01:52 +0530

    Add files via upload

--- upload.js
+++ upload.js
@@ -1,7 +1,22 @@
+const image_input = document.querySelector("#file");
+
+image_input.addEventListener("change", function(){
+
+    const reader = new FileReader();
+    reader.addEventListener("load", function(){
+        const uploaded_image = reader.result;
+        document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+    });
+
+    reader.readAsDataURL(this.files[0]);
+    
+})
+
 document.getElementById('form').addEventListener('submit', function(e){
     e.preventDefault();
     const userFile = document.getElementById('file').files[0];
-    console.log(userFile)
+
+    console.log(userFile.value)
     const formData = new FormData();
     formData.append('file', userFile);
     fetch('http://3.22.114.219:8000/upload_image', {
@@ -11,11 +26,9 @@ document.getElementById('form').addEventListener('submit', function(e){
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        // document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
 
    document.getElementById("calories").innerHTML = `
    <div id="result" class="card col-5">
-   <img class="card-img-top" src=data/${userFile.name} alt="Card image cap">
   <div class="card-body">
     <p class="card-title">
     
